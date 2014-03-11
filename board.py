f1589f8ef6a059e2f6284ecf747f7a0c15e397ac
Lucas Simpson <lucassimpson05@gmail.com>
2014-03-10 22:03:15 -0700

    added slide def, fixed random range

--- board.py
+++ board.py
@@ -20,9 +20,25 @@ class Board:
             for b in range (len (self.values [a])):
                 if self.values [a][b] == 0:
                     valid += [[a,b]]
-        key = random.randint (0, len (valid))
+        key = random.randint (0, len (valid) - 1)
         self.values [valid [key][0]] [valid [key][1]] = 2
-    
+
+    def slide (self, nums_):
+        nums = [a for a in nums_]
+        for a in range (len (nums) - 1):
+            if nums [a] == nums [a+1]:
+                nums [a+1] = 0
+                nums [a] *= 2
+        for a in range (1, len (nums)):
+            pos = a
+            while (nums [pos] != 0):
+                if pos == 0 or nums [pos - 1] != 0:
+                    break
+                nums [pos - 1] = nums [pos]
+                nums [pos] = 0
+                pos -= 1
+        return nums
+
     def __str__ (self):
         r = ''
         for a in self.values:
@@ -33,4 +49,4 @@ class Board:
 
 
 board = Board ()
-print board
+print board.slide ([0, 2, 2, 0])
